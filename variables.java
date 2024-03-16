904a12b083b4d97aef5e09f35de5a8f6a5663c6b
Shivansh Harbola <92177666+shiviii43@users.noreply.github.com>
2024-03-16 16:04:47 +0530

    Add files via upload

---
+++ variables.java
@@ -0,0 +1,24 @@
+package Learn;
+
+public class variables {
+    public static void main(String[] args)
+    {
+        byte age = 34;
+        int age2 = 56;
+        short age3 = 87;
+        long ageDino = 5666666666666L;
+        char ch = 'A';
+        float f1 = 5.6f;    
+        boolean a = true;
+
+        System.out.println(age);
+        String str = "Java Programming";
+        System.out.println(str);
+        System.out.println(age2);
+        System.out.println(age3);
+        System.out.println(ageDino);
+        System.out.println(ch);
+        System.out.println(f1);
+        System.out.println(a);
+    }
+}
